41c57749b7607f1898caa42c8dd6bb1b6a6e6175
vrlll <bicycle548@gmail.com>
2016-05-17 12:11:39 +0800

    add download (modified form data)
    change data (last time forgot the colon), change index (download button and draw button )

--- download.php
+++ download.php
@@ -11,6 +11,7 @@
     $link=mysqli_connect("localhost","bgbim","bgbimdb") or die ("無法開啟Mysql資料庫連結"); //建立mysql資料庫連結
     mysqli_select_db($link, "bgbim"); //選擇資料庫bgbim
 
+    //only grab time info fron weather station
     $myquery1 = "
 SELECT  'date', 'time'";
     $checkOutput = false;
@@ -24,10 +25,12 @@ SELECT  'date', 'time'";
         }
     }
     if($checkOutput == false)
-        $myquery1 = '';
+        $myquery1 = "
+SELECT  'date', 'time' FROM Weather_Station;";
     else{
         $myquery1 .= 'FROM Weather_Station;';
     }
+    
 
     $myquery2 = "
 SELECT  'date', 'time'";
@@ -62,7 +65,7 @@ SELECT  'date', 'time'";
             $myquery2 = '';
         else{
             $myquery2 .= 'FROM Part_1;';
-        }
+        }   
     }
     
 
@@ -120,13 +123,42 @@ SELECT  'date', 'time'";
         die;
     }
     
-    $data = array();
+    //$data = array();
     
+
+    //***these code  partly reference http://code.stephenmorley.org/php/creating-downloadable-csv-files/
+    // output headers so that the file is downloaded rather than displayed
+    header('Content-Type: text/csv; charset=utf-8');
+    header('Content-Disposition: attachment; filename=data.csv');
+
+    // create a file pointer connected to the output stream
+    $output = fopen('php://output', 'w');
+
     for ($x = 0; $x < mysql_num_rows($query); $x++) {
-        $data[] = mysql_fetch_assoc($query);
+        $data = mysql_fetch_assoc($query);
+        // output the column headings
+        fputcsv($output, $data);
     }
-    
-    echo json_encode($data);     
+
+    fclose($out);
+
+/*
+    // fetch the data
+    mysql_connect('localhost', 'username', 'password');
+    mysql_select_db('database');
+    $rows = mysql_query('SELECT field1,field2,field3 FROM table');
+
+    // loop over the rows, outputting them
+    while ($row = mysql_fetch_assoc($rows)) fputcsv($output, $row);
+    //***
+*/
+
+/*
+    $out = fopen('http://www.bgbim.tw/GreenRoof/outputdata.csv', 'w');
+    fputcsv($out, $data);
+    fclose($out);
+*/
+   // echo json_encode($data);     
      
     mysql_close($server);
 ?>
--- index.php
+++ index.php
@@ -13,6 +13,46 @@
 			if($_SESSION['login']=='OK');
 			else echo "<style>.secure{visibility:hidden;}</style>"; 		
 		?>
+		<script type="text/javascript" src="d3.js"></script>
+    <style>
+    .axis {
+        font-family: arial;
+        font-size:0.6em;
+    }
+    path {
+        fill:none;
+        stroke:black;
+        stroke-width:2px;
+    }
+    .tick {
+        fill:none;
+        stroke:black;
+    }
+    circle{
+        stroke:black;
+        stroke-width:0.5px;
+    }
+    circle.times_square{
+        fill:DeepPink;
+    }
+    circle.grand_central{
+        fill:MediumSeaGreen;
+    }
+    path.times_square{
+        stroke:DeepPink;
+    }
+    path.grand_central{
+        stroke:MediumSeaGreen;
+    }
+    
+    
+    
+    </style>
+    
+
+
+
+
 	</head>
 	<body>
 		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -53,7 +93,7 @@
 						<input type="file" name="fileToUpload"  id="fileToUpload"style="width:50%;float:left"></input><br>
 						<input type="submit" value="Upload Image" name="submit" class="btn btn-sm btn-primary" style="float:left"></input><br>
 					</form>
-					<br><br>
+					</br>
 					<form id="timeForm" class="secure">
 						Starting Time:<br>
 						<select id="startH"></select>
@@ -63,9 +103,10 @@
 						<select id="endH"></select>
 						<select id="endD"></select>
 						<select id="endM"></select><br>
-						<input type="button" id="download" value="Download" class="btn btn-primary" ></input>
-						<input type="button" id="draw" value="Draw" class="btn btn-primary"> </input><br>
+						<input type="button" id="download" value="Download" formaction="download.php" ></input>
+						<input type="button" id="draw" value="Draw" onclick="inputData()"> </input><br>
 					</form>
+					</br>
 
 					<script>
 						function changeDisplay(){
@@ -85,7 +126,6 @@
 						}
 					</script>
 
-					<input type="submit" name="Draw_Button" value="Draw" id="Draw_Button" class="myButton" />
 					 <select name="chooseTableList" id="chooseTableList" onchange="changeDisplay()">
 						<option selected="selected" value="Weather_Station">Weather_Station</option>
 						<option value="Green_Roof_A" >Green_Roof_A</option>
@@ -165,91 +205,116 @@
 							echo "Welcome to GRDS of BGBIM TW,". $_SESSION['nick'];
 							
 					}?>
-					<style>
-
-					svg {
-					  font: 10px sans-serif;
-					  shape-rendering: crispEdges;
-					}
-
-					rect {
-					  fill: #ddd;
-					}
-
-					.axis path,
-					.axis line {
-					  fill: none;
-					  stroke: #fff;
-					}
-
-					</style>
 					<script src="//d3js.org/d3.v3.min.js"></script>
 					<script>
-					function drawD3(){
-					    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-					        width = 960 - margin.left - margin.right,
-					        height = 500 - margin.top - margin.bottom;
+				    function draw(data) {
+				        "use strict";
+				        var margin = 50,
+				            width = 700 - margin,
+				            height = 300 - margin;
+				        
+				        var count_extent = d3.extent(
+				            data.times_square.concat(data.grand_central),
+				            function(d){return d.count}
+				        );
 
-					    var x = d3.scale.linear()
-					        .domain([-width / 2, width / 2])
-					        .range([0, width]);
+				        var time_extent = d3.extent(
+				            data.times_square.concat(data.grand_central),
+				            function(d){return d.time}
+				        )
+				        
+				        var count_scale = d3.scale.linear()
+				            .domain(count_extent)
+				            .range([height, margin]);
+				        
+				        var time_scale = d3.time.scale()
+				            .domain(time_extent)
+				            .range([margin, width])
+				        
+				        var time_axis = d3.svg.axis()
+				            .scale(time_scale)
+				        
+				        var count_axis = d3.svg.axis()
+				            .scale(count_scale)
+				            .orient("left")
+				        
+				        var line = d3.svg.line()
+				            .x(function(d){return time_scale(d.time)})
+				            .y(function(d){return count_scale(d.count)})
+				            .interpolate("linear")
+				                    
+				        d3.select("#main") 
+				          .append("svg")
+				            .attr("class","chart") 
+				            .attr("width", width+margin)
+				            .attr("height", height+margin)
+				        
+				        d3.select('svg')
+				          .append('path')
+				            .attr('d', line(data.times_square))
+				            .attr('class', 'times_square')
+				        
+				        d3.select('svg')
+				          .append('path')
+				            .attr('d', line(data.grand_central))
+				            .attr('class', 'grand_central')
+				        
+				        
+				        d3.select("svg")
+				          .selectAll("circle.times_square")
+				          .data(data.times_square)
+				          .enter()
+				          .append("circle")
+				            .attr("class", "times_square")
 
-					    var y = d3.scale.linear()
-					        .domain([-height / 2, height / 2])
-					        .range([height, 0]);
-
-					    var xAxis = d3.svg.axis()
-					        .scale(x)
-					        .orient("bottom")
-					        .tickSize(-height);
-
-					    var yAxis = d3.svg.axis()
-					        .scale(y)
-					        .orient("left")
-					        .ticks(5)
-					        .tickSize(-width);
-
-					    var zoom = d3.behavior.zoom()
-					        .x(x)
-					        .y(y)
-					        .scaleExtent([1, 32])
-					        .on("zoom", zoomed);
-
-					    var svg = d3.select("body").append("svg")
-					        .attr("width", width + margin.left + margin.right)
-					        .attr("height", height + margin.top + margin.bottom)
-					      .append("g")
-					        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-					        .call(zoom);
-
-					    svg.append("rect")
-					        .attr("width", width)
-					        .attr("height", height);
-
-					    svg.append("g")
-					        .attr("class", "x axis")
-					        .attr("transform", "translate(0," + height + ")")
-					        .call(xAxis);
-
-					    svg.append("g")
-					        .attr("class", "y axis")
-					        .call(yAxis);
-
-					    function zoomed() {
-					      svg.select(".x.axis").call(xAxis);
-					      svg.select(".y.axis").call(yAxis);
-					    }
+				        d3.select("svg")
+				          .selectAll("circle.grand_central")
+				          .data(data.grand_central)
+				          .enter()
+				          .append("circle")
+				            .attr("class", "grand_central")
+				    
+				        d3.selectAll("circle")
+				            .attr("cy", function(d){return count_scale(d.count);})
+				            .attr("cx", function(d){return time_scale(d.time);})
+				            .attr("r", 3)
+				            
+				        d3.select("svg")
+				          .append("g")
+				          .attr("class", "x axis")
+				          .attr("transform", "translate(0," + height + ")")
+				          .call(time_axis);
+				         
+				        d3.select("svg")
+				          .append("g")
+				          .attr("class", "y axis")
+				          .attr("transform", "translate(" + margin + ",0)")
+				          .call(count_axis);
+				    
+				      d3.select('.y.axis')
+				          .append('text')
+				          .text('mean number of turnstile revolutions')
+				          .attr('transform', "rotate (90, " + -margin + ", 0)")
+				          .attr('x', 20)
+				          .attr('y', 0)
+				                    
+				      d3.select('.x.axis')
+				        .append('text')
+				          .text('time')
+				          .attr('x', function(){return (width / 1.6) - margin})
+				          .attr('y', margin/1.5)
+				    
+				    }
+				    function inputData(){
+				    	d3.json('http://140.112.12.21/vie2015/data/turnstile_traffic.json', draw);
 					}
-					drawD3();
-					//d3.json("data.php", drawD3());
-					/*
-					d3.json("data.php", function(error, data) {
-					    data.forEach(function(d) {
-					        d.date = parseDate(d.date);
-					        d.close = +d.close;
-					    });
-					*/
-					</script>
+				    </script>
+				    <!--
+					<script>
+        			
+        			//draw();
+    				</script>-->
+
 
 
 				</div>
